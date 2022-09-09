3154e2740fe300bec67c441bab6402c7d011a9c2
mbuguakenneth <mbuguakenneth97@gmail.com>
2022-09-09 12:15:40 +0300

    Updates

---
+++ precache-manifest.d23122e689f6eddf66d65b943b006951.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "21d250b2597eb3766413",
+    "url": "/myport/static/css/main.bc66523e.chunk.css"
+  },
+  {
+    "revision": "21d250b2597eb3766413",
+    "url": "/myport/static/js/main.21d250b2.chunk.js"
+  },
+  {
+    "revision": "9c06dd577fc89baa13ed",
+    "url": "/myport/static/css/1.efff3f00.chunk.css"
+  },
+  {
+    "revision": "9c06dd577fc89baa13ed",
+    "url": "/myport/static/js/1.9c06dd57.chunk.js"
+  },
+  {
+    "revision": "0afd612b33fdb446188b",
+    "url": "/myport/static/js/runtime~main.0afd612b.js"
+  },
+  {
+    "revision": "a0fb72e84830ca0d697f1ff946e30725",
+    "url": "/myport/static/media/moviesite.a0fb72e8.png"
+  },
+  {
+    "revision": "603e705246cd7c1b95714993b33f0146",
+    "url": "/myport/static/media/kenntube.603e7052.png"
+  },
+  {
+    "revision": "4a6558007113616ab2fd33ab3976a662",
+    "url": "/myport/static/media/lanedetection.4a655800.png"
+  },
+  {
+    "revision": "eaf6f053feef03d27498c89175e80046",
+    "url": "/myport/static/media/portfolio.eaf6f053.png"
+  },
+  {
+    "revision": "f7d742fc14d3d495da2581043372d15a",
+    "url": "/myport/static/media/fastapi_sklearn.f7d742fc.png"
+  },
+  {
+    "revision": "23273bc8bc3197e29e433d7cf66185fc",
+    "url": "/myport/static/media/sklearn_genetic_opt.23273bc8.png"
+  },
+  {
+    "revision": "f87d14d0fab42b93475f41f907237a8d",
+    "url": "/myport/index.html"
+  }
+];
